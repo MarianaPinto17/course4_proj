7b8843b1c954e8d403539ace60e1bbf14774b9d8
Mariana Pinto <mipinto@ubiwhere.com>
2023-05-22 06:22:18 +0000

    Finish scheduling with celery

--- course4_proj/urls.py
+++ course4_proj/urls.py
@@ -21,10 +21,8 @@ import movies.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", gh.views.index)
+    path("", gh.views.index),
     path("search/", movies.views.search, name="search"),
-    path(
-        "search-wait/<uuid:result_uuid>/", movies.views.search_wait, name="search_wait"
-    ),
-    path("search-results/", movies.views.search_results, name="search_results")
+    path("search-wait/<uuid:result_uuid>/", movies.views.search_wait, name="search_wait"),
+    path("search-results/", movies.views.search_results, name="search_results"),
 ]
